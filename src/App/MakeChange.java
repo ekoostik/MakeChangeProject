183c9f98e6cd8c888827aa321a79f2721ae926d2
David Dunlevy <david.dunlevy@gmail.com>
2023-01-15 09:00:48 +1300

    Getting cleaner

--- src/App/MakeChange.java
+++ src/App/MakeChange.java
@@ -44,18 +44,19 @@ public class MakeChange {
 
 		if (cost > paid) {
 			double stillOwe = cost - paid;
-			System.out.println("The customer still owes $" + stillOwe);
+			System.err.println("The customer still owes $" + stillOwe);
 			return stillOwe;
-		} 
-		else if (cost < paid) {
+		} else if (cost < paid) {
 			double change = paid - cost;
-			System.out.println(" Customer is owed $" + change + " in change.");
+			System.out.println("Customer is owed $" + change + " in change.");
 
-			double tens = (int)change / 10;
+			double twenty = (int) change / 20;
+			change = change % 20;
+			double tens = (int) change / 10;
 			change = change % 10;
-			double fives = (int)change / 5;
+			double fives = (int) change / 5;
 			change = change % 5;
-			double single = (int)change / 1;
+			double single = (int) change / 1;
 			change = change % 1;
 			double quart = change / .25;
 			change = change % .25;
@@ -67,37 +68,73 @@ public class MakeChange {
 			double penn = (change + .005) * 100;
 			change = change % .01;
 
+			if (twenty != 0) {
+				if (twenty > 1) {
+					System.out.println((int) twenty + " :Twenty dollar bills. ");
+				} else {
+					System.out.println((int) twenty + " :Twenty dollar bill. ");
+				}
+			}
 			if (tens != 0) {
-				System.out.print((int) tens + " :Ten dollar bill, ");
+				if (tens > 1) {
+					System.out.print((int) tens + " :Ten dollar bills. ");
+				} else {
+					System.out.print((int) tens + " :Ten dollar bill. ");
+				}
 			}
 			if (fives != 0) {
-				System.out.print((int) fives + " :Five dollar bill, ");
+				if (fives > 1) {
+					System.out.print((int) fives + " :Five dollar bills. ");
+				} else {
+					System.out.print((int) fives + " :Five dollar bill. ");
+				}
 			}
-			if (single  != 0) {
-				System.out.print((int) single + " :One dollar bill, ");
+			if (single != 0) {
+				if (single > 1) {
+					System.out.print((int) single + " :One dollar bills. ");
+				} else {
+					System.out.print((int) single + " :One dollar bill. ");
+				}
 			}
 			if (quart != 0 && quart > 1) {
-				System.out.print((int) quart + " :Quarter, ");
+				if ((int)quart > 1) {
+					System.out.print((int) quart + " :Quarters. ");
+				} else {
+					System.out.print((int) quart + " :Quarter. ");
+				}
 			}
 			if (dime != 0 && dime > 1) {
-				System.out.print((int) dime + " :Dime, ");
+				if ((int)dime > 1) {
+					System.out.print((int) dime + " :Dimes. ");
+				} else {
+					System.out.print((int) dime + " :Dime. ");
+				}
+
 			}
 			if (nick != 0 && nick > 1) {
-				System.out.print((int) nick + " :Nickel, ");
+				if ((int)nick > 1) {
+					System.out.print((int) nick + " :Nickels. ");
+				} else {
+					System.out.print((int) nick + " :Nickel. ");
+				}
+
 			}
-			if (penn != 0) {
-				System.out.print((int)penn + " :Penny, ");
+			if (penn != 0 && penn > 1) {
+				if ((int)penn > 1) {
+					System.out.print((int) penn + " :Pennies. ");
+				} else {
+					System.out.print((int) penn + " :Penny. ");
+				}
+
 			}
 			return change;
 		}
-	
-			
-			else {
+
+		else {
 			double perfect = 0;
 			System.out.println("Perfect! Exact payment, take a 10 min break!");
 			return perfect;
 		}
-	
 
-}
+	}
 }
