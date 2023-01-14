59f4ec1513fef1bd2c38675bd2b63169f57fa6a2
David Dunlevy <david.dunlevy@gmail.com>
2023-01-14 15:13:08 +1300

    User1 comp

--- src/App/MakeChange.java
+++ src/App/MakeChange.java
@@ -1,40 +1,37 @@
 package App;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
+		double price = itemCost();
+		
+		
+//		System.out.println(price);
 
 	}
 
-	
 // #1
-	
-	
-	
-	
-	
+
+	public static double itemCost() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Hello! welcome to the Make Change App!");
+		System.out.print("Please enter the item price: ");
+		double cost = sc.nextDouble();
+		sc.close();
+		return cost;
+		
+
+	}
 
 //#2
-	
-	
-	
-	
-	
-	
-	
 
 //#3
-	
-	
-	
-	
-	
 
 // #4
 
-	
-	
-	
 	
 	
 }
