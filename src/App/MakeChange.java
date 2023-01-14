a71e2cbd9dac97e871dc44d1c7e7d045cda9bccb
David Dunlevy <david.dunlevy@gmail.com>
2023-01-15 07:50:32 +1300

    Working, dirty

--- src/App/MakeChange.java
+++ src/App/MakeChange.java
@@ -9,8 +9,7 @@ public class MakeChange {
 		double paid = moneyGivin();
 		double change = prompt(price, paid);
 
-
-		// System.out.println(price);
+// 		System.out.println(price);
 //		System.out.println(paid);
 	}
 
@@ -34,6 +33,7 @@ public class MakeChange {
 		System.out.print("Now enter the amount the customer paid: ");
 		double paid = sc.nextDouble();
 		sc.nextLine();
+		sc.close();
 		return paid;
 
 	}
@@ -44,20 +44,60 @@ public class MakeChange {
 
 		if (cost > paid) {
 			double stillOwe = cost - paid;
-
 			System.out.println("The customer still owes $" + stillOwe);
 			return stillOwe;
-		} else if (cost < paid) {
+		} 
+		else if (cost < paid) {
 			double change = paid - cost;
-			System.out.println("You owe the customer $" + change);
+			System.out.println(" Customer is owed $" + change + " in change.");
+
+			double tens = (int)change / 10;
+			change = change % 10;
+			double fives = (int)change / 5;
+			change = change % 5;
+			double single = (int)change / 1;
+			change = change % 1;
+			double quart = change / .25;
+			change = change % .25;
+			double dime = change / .1;
+			change = change % .1;
+			double nick = change / .05;
+			change = change % .05;
+//			System.out.println(change);
+			double penn = (change + .005) * 100;
+			change = change % .01;
+
+			if (tens != 0) {
+				System.out.print((int) tens + " :Ten dollar bill, ");
+			}
+			if (fives != 0) {
+				System.out.print((int) fives + " :Five dollar bill, ");
+			}
+			if (single  != 0) {
+				System.out.print((int) single + " :One dollar bill, ");
+			}
+			if (quart != 0 && quart > 1) {
+				System.out.print((int) quart + " :Quarter, ");
+			}
+			if (dime != 0 && dime > 1) {
+				System.out.print((int) dime + " :Dime, ");
+			}
+			if (nick != 0 && nick > 1) {
+				System.out.print((int) nick + " :Nickel, ");
+			}
+			if (penn != 0) {
+				System.out.print((int)penn + " :Penny, ");
+			}
 			return change;
-		} else {
+		}
+	
+			
+			else {
 			double perfect = 0;
 			System.out.println("Perfect! Exact payment, take a 10 min break!");
 			return perfect;
 		}
-	}
+	
 
-// #4
-
+}
 }
